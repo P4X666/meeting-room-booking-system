ad6724f5ce38b87a51d41a053be60de7634d239a
pengshixing <pengshixing17347856051@outlook.com>
2026-05-21 15:56:52 +0800

    feat: 连接mysql，添加表设计

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { User } from './user/entities/user.entity';
+import { Role } from './user/entities/role.entity';
+import { Permission } from './user/entities/permission.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'root',
+      password: '123456',
+      database: 'meeting_room_booking_system',
+      synchronize: true,
+      logging: true,
+      entities: [User, Role, Permission],
+      poolSize: 10,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
